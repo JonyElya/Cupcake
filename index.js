a2088481243773f1eaf2f04a8c4169cb52220fe5
JonyElya <38189777+JonyElya@users.noreply.github.com>
2020-03-17 01:58:43 +0700

    добавил первоначальную конфигурацию

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Container from "./src/components/container";
+
+ReactDOM.render(<Container />, document.getElementById("root"));
