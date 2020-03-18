82817b2f9922de02d739eb930435ae70b604a913
JonyElya <38189777+JonyElya@users.noreply.github.com>
2020-03-18 15:15:19 +0700

    Обновил модули

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,10 @@ module.exports = {
   // A SourceMap without column-mappings ignoring loaded Source Maps.
   devtool: "cheap-module-source-map",
   plugins: [
+    
     //simplifies creation of HTML files to serve your webpack bundles. This is especially useful for webpack bundles that include a hash in the filename which changes every compilation. You can either let the plugin generate an HTML file for you, supply your own template using lodash templates or use your own loader.
     new HtmlWebpackPlugin({
-      title: "Hot Module Replacement"
+      title: "Cupcake"
     }),
     //Auto replacement of page when i save some file, even css
     new webpack.HotModuleReplacementPlugin()
@@ -39,10 +40,23 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-        include: /flexboxgrid/
-        //Follow instructions at https://github.com/roylee0704/react-flexbox-grid
+        // For pure CSS (without CSS modules)
+        test: /\.css$/i,
+        exclude: /\.module\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        // For CSS modules
+        test: /\.module\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
