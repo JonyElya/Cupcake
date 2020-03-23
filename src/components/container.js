6ccbb06ccac4193f007ceeb226c85d64cc8ca8c1
JonyElya <38189777+JonyElya@users.noreply.github.com>
2020-03-23 08:52:44 +0700

    Добавлены route

--- src/components/container.js
+++ src/components/container.js
@@ -1,13 +1,21 @@
-import React, { Component } from "react";
-import BooksContainer from "./Books/booksContainer";
-import Header from "./Header/header";
+import React from "react";
 import style from "./main.module.css";
+import BooksContainer from "./Books/booksContainer";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Cart from "./Cart/cart";
+import NavbarContainer from "./Navbar/navbarConatiner";
 
 const Container = () => {
   return (
     <div className={style.Container}>
-      <Header />
-      <BooksContainer />
+      <React.Fragment>
+        <NavbarContainer />
+        <Switch>
+          <Route path="/basket-cart" component={Cart} />
+          <Route path="/home" component={BooksContainer} />
+          <Redirect from="/" exact to="/home" />
+        </Switch>
+      </React.Fragment>
     </div>
   );
 };
