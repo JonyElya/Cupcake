54ee8848f42d43d10424af6dbda091c7042715ee
JonyElya <38189777+JonyElya@users.noreply.github.com>
2020-03-23 08:55:28 +0700

    Добавил модальное окно

--- src/components/Books/books.js
+++ src/components/Books/books.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Book from "./book/book";
+import BookModal from "./book/bookModal";
 import style from "./books.module.css";
+import BookContainer from "./book/bookContainer";
 
 const Books = props => {
   return (
     <div className={style.BookContent}>
       {props.books.map((book, i) => (
-        <Book key={i} book={book} />
+        <BookContainer key={i} book={book} />
       ))}
     </div>
   );
