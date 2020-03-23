21aebb37957c6538515a83e4900f976feffe3734
JonyElya <38189777+JonyElya@users.noreply.github.com>
2020-03-23 08:54:05 +0700

    Добавил reactstrap и bootstrap

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import Container from "./src/components/container";
 import store from "./redux/reduxStore";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <BrowserRouter>
